9b981a3224df451531ed23e879917abe61f01198
Mohammed Naguib <m.naguib2611@gmail.com>
2025-02-10 00:10:35 +0000

    feat: Replace app service with Jest and introduceMySelf to return developer's name

--- src/app.service.spec.ts
+++ src/app.service.spec.ts
@@ -1,45 +1,40 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */ 
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppService } from './app.service';
+import * as jest from 'jest';
+import { AppModule } from './app.module';
 
 describe('AppService', () => {
   let appService: AppService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AppService],
+      imports: [AppModule],
     }).compile();
 
     appService = module.get<AppService>(AppService);
   });
 
-  describe('getHello', () => {
-    it('should return Hello World!', () => {
-      expect(appService.getHello()).toBe('Hello World!');
-    });
-
-    it('should return an error for empty input', () => {
-      expect(appService.getHello()).toBeError();
-    });
+  it('should return "Hello World!" when getHello() is called', () => {
+    expect(appService.getHello()).toBe('Hello World!');
   });
 
-  describe('introduceMySelf', () => {
-    it('should return My name is Mohammed!', () => {
-      expect(appService.introduceMySelf()).toBe('My name is Mohammed!');
-    });
-
-    it('should return an error for empty input', () => {
-      expect(appService.introduceMySelf()).toBeError();
-    });
+  it('should return "My name is Mohammed!" when introduceMySelf() is called', () => {
+    expect(appService.introduceMySelf()).toBe('My name is Mohammed!');
   });
 
-  describe('introduceYourSelf', () => {
-    it('should return My name is LM studio!', () => {
-      expect(appService.introduceYourSelf()).toBe('My name is LM studio!');
-    });
+  it('should return "Your name is Mohammed!" when introduceYourSelf() is called', () => {
+    expect(appService.introduceYourSelf()).toBe('My name is Mohammed!');
+  });
 
-    it('should return an error for empty input', () => {
-      expect(appService.introduceYourSelf()).toBeError();
-    });
+  it('should return error when getHello() is called with invalid input', async () => {
+    expect(() => appService.getHello()).toThrowError('Expected function call to be a string, but got an undefined value');
+  });
+
+  it('should return error when introduceMySelf() is called with invalid input', async () => {
+    expect(() => appService.introduceMySelf()).toThrowError('Expected function call to be a string, but got an undefined value');
+  });
+
+  it('should return error when introduceYourSelf() is called with invalid input', async () => {
+    expect(() => appService.introduceYourSelf()).toThrowError('Expected function call to be a string, but got an undefined value');
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,16 +1,34 @@
 import { Injectable } from '@nestjs/common';
 
+/**
+ * The AppService class provides methods to return greetings messages.
+ */
 @Injectable()
 export class AppService {
+  /**
+   * Returns a greeting message when the user says hello.
+   *
+   * @returns The greeting message, which is always 'Hello World!'.
+   */
   getHello(): string {
     return 'Hello World!';
   }
 
+  /**
+   * Returns a greeting message introducing the developer with his full name.
+   *
+   * @returns The introduction message, which is currently incorrect.
+   */
   introduceMySelf(): string {
     return 'My name is Mohammed!';
   }
 
+  /**
+   * Returns a greeting message with the developer's actual name.
+   *
+   * @returns The introduction message, which was previously incorrect.
+   */
   introduceYourSelf(): string {
-    return 'My name is LM studio!';
+    return 'My name is Mohammed!';
   }
 }
